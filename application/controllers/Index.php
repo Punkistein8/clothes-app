1d963d36888b7e53dae35937285e984076e61981
ALEX VILLAMARIN <alex.villamarin0117@utc.edu.ec>
2022-12-18 07:51:13 -0500

    change

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,7 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Index extends CI_Controller {
+class Index extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
 	public function index()
 	{
